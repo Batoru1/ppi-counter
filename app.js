a89e39ee5e70bbef3b9fc4074c916edba484da36
Tomas <103144015+Batoru1@users.noreply.github.com>
2023-12-29 18:53:13 +0200

    index.html

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const ppi = function (a, b) {
-  return Math.sqrt(widthRes ** 2 + heightRes ** 2) / diagonal;
+const countPpi = function (width, height, diagonal) {
+  return Math.sqrt(width ** 2 + height ** 2) / diagonal;
 };
 
-let widthRes;
-let heightRes;
-let diagonal;
+// let widthRes;
+// let heightRes;
+// let diagonal;
 
-widthRes = 1920;
-heightRes = 1080;
-diagonal = 15.6;
+// widthRes = 1920;
+// heightRes = 1080;
+// diagonal = 15.6;
 
-ppi(1920, 1080);
+// countPpi();
 
-console.log(ppi());
+console.log(countPpi(2560, 1440, 32));
+
+const widthRes = function () {
+  const inputWidth = document.getElementsByClassName('width');
+  const width = inputWidth.value;
+};
