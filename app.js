71eec4c390dfa195bcab4ef3527b54e6d4681f77
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-01-11 15:38:16 +0200

    functional, eset work

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// let widthRes;
-// let heightRes;
-// let diagonal;
-
-// widthRes = 1920;
-// heightRes = 1080;
-// diagonal = 15.6;
-
-// countPpi();
-
-// const countPpi = function (widthRes, heightRes, diagonal) {
-//   return Math.sqrt(widthRes ** 2 + heightRes ** 2) / diagonal;
-// };
-// console.log(countPpi(2560, 1440, 32));
-
 const countPpi = function (width, height, diagonal) {
   return Math.sqrt(width ** 2 + height ** 2) / diagonal;
 };
@@ -27,4 +12,12 @@ document.querySelector('.btn').addEventListener('click', function () {
     heightRes,
     diagonal
   );
+});
+
+document.querySelector('.reset').addEventListener('click', function () {
+  let widthRes = Number((document.querySelector('.dataW').value = ''));
+  let heightRes = Number((document.querySelector('.dataH').value = ''));
+  let diagonal = Number((document.querySelector('.dataD').value = ''));
+
+  document.querySelector('.result').textContent = '?';
 });
